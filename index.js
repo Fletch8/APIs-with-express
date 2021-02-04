8c9a10dd152aa427b7596535954e747e5b475b05
Fletch S <stampsfj04@gmail.com>
2021-02-04 15:20:05 -0500

    added swapi api to show.ejs

--- index.js
+++ index.js
@@ -1,24 +1,105 @@
-// gives access to varibles in our .env (ex: API_KEY)
-require('dotenv').config()
+// // gives access to varibles in our .env (ex: API_KEY)
+// require('dotenv').config()
 
-// to see env varibles
+// // to see env varibles
+// // console.log(process.env)
+// // console.log(process.env.API_KEY)
+// const express = require('express')
+// const app = express()
+// const axios = require('axios')
+// const expressLayouts = require('express-ejs-layouts')
+// const methodOverride = require('method-override')
+
+
+// // Middleware
+// app.use(expressLayouts)
+// app.set('view-engine', 'ejs')
+// app.use(express.urlencoded({extended: false}))
+// app.use(methodOverride('_method'))
+
+// //routes
+// app.get('/', (req, res) => {
+//     res.send('hello there..')
+// })
+
+// app.get('/omdb', (req, res)=> {
+//     const qs= {
+//         params:{
+//             s: 'star wars',
+//             apikey: process.env.API_KEY
+//         }
+//     }
+//     axios.get('http://www.omdbapi.com', qs)
+//     .then((response) => {
+//         console.log(response.data)
+
+//     })
+// })
+
+
+// app.get('/swapi/search', (req, res) => {
+//     res.render('search')
+// })
+
+// app.get('/swapi/show', (req,res) => {
+//     console.log('made it here!')
+// })
+
+// const PORT = process.env.PORT || 3000
+// app.listen(PORT, console.log(`listening on Port: ${PORT}`))
+
+// gives us access to variables in our .env
+require('dotenv').config()
+// see variables using process.env
 // console.log(process.env)
 // console.log(process.env.API_KEY)
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const expressLayouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
-
-// Middleware
+// MiddleWare
 app.use(expressLayouts)
-app.set('view-engine', 'ejs')
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
-
-//routes
-app.get('/', (req, res) => {
-    res.send('hello there..')
+// Routes
+app.get('/', (req, res)=> {
+    res.send('hello')
 })
-
+app.get('/omdb', (req, res)=> {
+    const qs= {
+        params: {
+            s: 'star wars',
+            apikey: process.env.API_KEY
+        }
+    }
+    // http://www.omdbapi.com/?s=star+wars&apikey=OUR+key
+    axios.get('http://www.omdbapi.com', qs)
+    .then((response)=> {
+        console.log(response.data)
+    })
+})
+// with the swapi api
+app.get('/swapi/search', (req, res)=> {
+  res.render('search')
+})
+app.get('/swapi/show', (req, res)=> {
+   console.log('made it here') 
+   console.log('query', req.query)
+   // basic api call to get a person
+   axios.get(`https://swapi.dev/api/people/${req.query.personId}`)
+   .then((response)=> {
+       // response.data is where our data lives
+       console.log(response.data)
+       // make a person object
+       const person = {
+           name: response.data.name,
+           birth: response.data['birth_year'],
+           home: response.data.homeworld
+       }
+       res.render('show', person)
+   })
+})
 const PORT = process.env.PORT || 3000
-app.listen(PORT, console.log(`listening on Port: ${PORT}`))
+app.listen(PORT, console.log(`Listening on ${PORT}`))
